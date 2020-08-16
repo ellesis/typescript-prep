8857d2a3e5dff553659cf0270859c05e49c483e0
Lisa Han <ellesis@gmail.com>
2020-08-16 12:15:24 -0700

    typescript samples

---
+++ ts-lecture-master/lecture/types.js
@@ -0,0 +1,18 @@
+export class Sub {
+    constructor(mine) {
+        this.mine = mine;
+        this.att = Math.ceil(Math.random() * 5);
+        this.hp = Math.ceil(Math.random() * 5);
+        this.cost = Math.floor((this.att + this.hp) / 2);
+        this.field = false;
+    }
+}
+export class Hero {
+    constructor(mine) {
+        this.mine = mine;
+        this.att = Math.ceil(Math.random() * 2);
+        this.hp = Math.ceil(Math.random() * 5) + 25;
+        this.hero = true;
+        this.field = true;
+    }
+}
